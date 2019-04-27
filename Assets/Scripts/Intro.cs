715538a2451be5b3181f9d8c3fde956319201e75
Nadia Morris <reversegremlin@gmail.com>
2019-04-27 12:29:44 -0400

    fixed webgl aspec ratio

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Intro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            LoadNextScene();
+        }
+
+    }
+
+    void LoadNextScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.buildIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings);
+        }
+    }
+
+}
